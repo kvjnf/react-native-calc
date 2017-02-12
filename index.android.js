5636691112ef5c62945364bb5e7167819ce8a6a4
kvjnf <dai.akiyam@gmail.com>
2017-02-12 23:48:05 +0900

    firstcommit

---
+++ index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {AppRegistry} from 'react-native';
+import ReactCalculator from './src/ReactCalculator';
+
+AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
