e41c54f86030de431d94a7039bead3ffe932be66
akiyama_daisuke <akiyama_daisuke@5star.co.jp>
2017-02-13 11:46:44 +0900

    チュートリアル終了

--- src/InputButton.js
+++ src/InputButton.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
 import {
   View,
-  Text
+  Text,
+  TouchableHighlight
 } from 'react-native';
 
 import Style from './Style';
 
 export default class InputButton extends Component {
   render(){
-    return(
-      <View style={Style.inputButton}>
-        <Text style={Style.inputButtonText}>{this.props.value}</Text>
-      </View>
-    )
+      return (
+          <TouchableHighlight style={[Style.inputButton, this.props.highlight ? Style.inputButtonHighlighted : null]}
+                              underlayColor="#193441"
+                              onPress={this.props.onPress}>
+              <Text style={Style.inputButtonText}>{this.props.value}</Text>
+          </TouchableHighlight>
+      )
   }
 }
